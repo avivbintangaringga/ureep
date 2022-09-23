5bba84b45c81998c1c271b29697f00d6a9f95d1b
Aviv Bintang Aringga <avivbintangaringga90@gmail.com>
2022-09-23 09:10:33 +0700

    First commit

---
+++ app/src/main/java/com/n00bz/ureep/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.n00bz.ureep
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
